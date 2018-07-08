d456c250ca0005030d0dcfe97d20cbe8fce1606c
meowoodie <woodielove@163.com>
2018-07-08 00:42:09 -0400

    update preproc function

--- scripts/preproc.R
+++ scripts/preproc.R
@@ -1,10 +1,23 @@
+# R functions for preprocessing census dataset collected from US Census Website.
 # 
-readCensusData = function (root.dir, category) {
-  censusPath = paste(c(root.dir, category, "/"), collapse = '')
-  years      = dir(census.path)
-  for (year in years) {
+# By Shixiang Zhu
+# Contact: shixiang.zhu@gatech.edu
+
+# Function for reading census data given specific category and factors over all 
+# the recentyears (all the data files under the category), this function will 
+# return a single dataframe which contains indicated factors of this category.
+read.census = function (root.dir, category, factors) {
+  census.path = paste(c(root.dir, category), collapse = '/')
+  for (year in dir(census.path)) {
     if (!grepl("metadata", year)) {
-      
+      year.census.path = paste(c(census.path, year), collapse = '/')
+      for (file in dir(year.census.path)) {
+        if (grepl("with_ann.csv", file)){
+          file.year.census.path = paste(c(year.census.path, file), collapse = '/')
+          rawdata = read.csv(file.year.census.path, header = FALSE, sep = ",")
+          break
+        }
+      }
     }
   }
 }
--- scripts/regression.R
+++ scripts/regression.R
@@ -5,18 +5,19 @@
 # By Shixiang Zhu
 # Contact: shixiang.zhu@gatech.edu
 
-root.dir   = "Desktop/workspace/Atlanta-Zoning/data/census/"
-# years      = c("11", "12", "13", "14", "15")
+root.dir   = "Desktop/workspace/Atlanta-Zoning"
+data.dir   = paste(root.dir, "data/census", sep="/")
 # categories = c("Educational Attainment", "Employment Status", "Marital Status", "population, age and sex, race and ethnicity", "School Enrollment", 
 #                "income/EARNINGS IN THE PAST 12 MONTHS (INFLATION-ADJUSTED DOLLARS)")
-census.education.2011 = paste(c(root.dir, "/Educational Attainment/ACS_11_5YR_S1501/ACS_11_5YR_S1501_with_ann.csv"), collapse = '')
-
+source(paste(root.dir, "scripts/preproc.R", sep="/"))
 
 # Step 1.
 # Read data from local files. The dataset is organized hierarchically by the 
 # categories of census feature and years. The following code would traverse 
 # through the hierarchy of files and read the data into dataframes.
-census.education.rawdata = read.csv(census.education.2011, header = FALSE, sep = ",")
+read.census(data.dir, "Educational Attainment")
 
-names(dat) <- as.matrix(census.education.rawdata[1, ])
-dat <- dat[-1, ]
+# census.education.rawdata = read.csv(census.education.2011, header = FALSE, sep = ",")
+# 
+# names(dat) <- as.matrix(census.education.rawdata[1, ])
+# dat <- dat[-1, ]
