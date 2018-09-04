364a2b3075d7594eff304cdc0e4a841d7ef1a252
meowoodie <woodielove@163.com>
2018-09-04 11:30:37 -0400

    initial commit for simulation

---
+++ simulation/client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+
+import os
+import numpy as np
---
+++ simulation/region.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+
+
+
+class Region(object):
+    '''
+    '''
+
+    def __init__(self, coords):
+
+
+    def __str__(self):
+        return
+
+
+if __name__ == '__main__':
+    coords = [(0, 0), (1, 0), (0, 1), (1, 1)]
+    region = Region(coords)
+    print(region)
