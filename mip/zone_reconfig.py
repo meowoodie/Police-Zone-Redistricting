ab522aaec1b5c30102d9630ec1d280f30cc010cc
meowoodie <woodielove@163.com>
2019-01-29 21:23:55 -0500

    improve the results by 1. fix w; 2. change obj to linear.

--- mip/zone_reconfig.py
+++ mip/zone_reconfig.py
@@ -32,7 +32,7 @@ with open("../data/beats_graph.csv", newline="") as farcs, \
     centroids = { centroid[0]: [float(centroid[1]), float(centroid[2])] for centroid in centroids[1:] }
 
 n = len(nodes)
-q = n - m + 1 # the maximum number of beats to be chosen in a zone.
+q = 20 # n - m + 1 # the maximum number of beats to be chosen in a zone.
 
 # create gurobi model
 model = Model("Zone Reconfiguration")
@@ -41,41 +41,53 @@ model = Model("Zone Reconfiguration")
 # - decision variable: if node i is in zone k
 x = model.addVars(nodes, zones, name="x", vtype=GRB.BINARY)
 # - if beat i is selected as a sink in zone k
-w = model.addVars(nodes, zones, name="w", vtype=GRB.BINARY)
+h = model.addVars(nodes, zones, name="w", vtype=GRB.BINARY)
+h['114',0].lb = 1
+h['202',1].lb = 1
+h['313',2].lb = 1
+h['413',3].lb = 1
+h['503',4].lb = 1
+h['611',5].lb = 1
 # - the non-negative flow from node i to node j in zone k
-y = model.addVars(nodes, nodes, zones, name="y", vtype=GRB.CONTINUOUS)
+f = model.addVars(nodes, nodes, zones, name="y", vtype=GRB.CONTINUOUS)
 
 # Data
 # - workload in node i
-u = workloads
+w = workloads
 s = centroids
 
 # Constraints
 # - b: each node can only be allocated to one zone
 model.addConstrs(( x.sum(i,"*") == 1 for i in nodes ), "b")
 # - c: the net outflow from each node
-model.addConstrs(( 
-    sum([ y[i,j,k] for j in arcs[i] ]) - sum([ y[j,i,k] for j in arcs[i] ]) >= x[i,k] - q * w[i,k]
+model.addConstrs((
+    sum([ f[i,j,k] for j in arcs[i] ]) - sum([ f[j,i,k] for j in arcs[i] ]) >= x[i,k] - q * h[i,k]
     for i in nodes for k in zones ), "c")
 # - d: specify the number of nodes that can be used as sinks.
-model.addConstr(w.sum() == m, "d")
+model.addConstr(h.sum() == m, "d")
 # - e: ensure that each zone must have only one sink
-model.addConstrs(( w.sum("*",j) == 1 for j in zones ), "e")
+model.addConstrs(( h.sum("*",j) == 1 for j in zones ), "e")
 # - f: ensure that there is no flow into any node i from outside of zone k (where xik = 0), 
 #      and that the total inflow of any node in zone k (where xik = 1) does not exceed q − 1.
-model.addConstrs(( sum([ y[j,i,k] for j in arcs[i] ]) <= (q - 1) * x[i,k] for i in nodes for k in zones ), "f")
+model.addConstrs(( sum([ f[j,i,k] for j in arcs[i] ]) <= (q - 1) * x[i,k] for i in nodes for k in zones ), "f")
 # - g: ensure unless a node i is included in zone k, the node k cannot be a sink in zone k.
-model.addConstrs(( w[i,k] - x[i,k] <= 0 for i in nodes for k in zones ), "g")
+model.addConstrs(( h[i,k] - x[i,k] <= 0 for i in nodes for k in zones ), "g")
 # - h, i: ensure that there is no flows (inflows and outflows) between different zones which forces eligible contiguity.
-model.addConstrs(( y[i,j,k] + y[j,i,k] <= (q - 1) * x[i,k] for i in nodes for j in nodes for k in zones ), "h")
-model.addConstrs(( y[i,j,k] + y[j,i,k] <= (q - 1) * x[j,k] for i in nodes for j in nodes for k in zones ), "i")
+model.addConstrs(( f[i,j,k] + f[j,i,k] <= (q - 1) * x[i,k] for i in nodes for j in nodes for k in zones ), "h")
+model.addConstrs(( f[i,j,k] + f[j,i,k] <= (q - 1) * x[j,k] for i in nodes for j in nodes for k in zones ), "i")
 # - j: non-negative net flow
-model.addConstrs(( y[i,j,k] >= 0 for i in nodes for j in nodes for k in zones ), "j")
+model.addConstrs(( f[i,j,k] >= 0 for i in nodes for j in nodes for k in zones ), "j")
 
-# objective 1: balancing workload between zones
-obj_balance_workload = sum([ 
-    (sum([ x[i,k] * u[i] for i in nodes ]) - sum([ u[i] for i in nodes ]) / m) * \
-    (sum([ x[i,k] * u[i] for i in nodes ]) - sum([ u[i] for i in nodes ]) / m) \
+# objective 1: balancing workload between zones in quadratic form.
+# obj_balance_workload = sum([ 
+#     (sum([ x[i,k] * w[i] for i in nodes ]) - sum([ w[i] for i in nodes ]) / m) * \
+#     (sum([ x[i,k] * w[i] for i in nodes ]) - sum([ w[i] for i in nodes ]) / m) \
+#     for k in zones ])
+# objective 1: balancing workload between zones in linear form.
+obj_balance_workload = sum([
+    sum([ x[i,k] * w[i] * w[i] for i in nodes ]) + 
+    2 * sum([ x[i,k] * x[j,k] * w[i] * w[j] for i in nodes for j in nodes if i != j ]) -
+    2 * sum([ x[i,k] * w[i] for i in nodes ]) * sum([ w[i] for i in nodes ]) / m
     for k in zones ])
 
 # objective 2: shape compactness
@@ -91,27 +103,18 @@ obj_balance_workload = sum([
 #     for i, ei in enumerate(nodes) for j, ej in enumerate(nodes) for k in zones if i > j ])
 
 # set objective for the model
-model.setObjective(obj_balance_workload, # + lam * obj_compactness, 
-    GRB.MINIMIZE)
+model.setObjective(obj_balance_workload, GRB.MINIMIZE)
 
 # Decision initialization
-b = []
-z = []
 for i in nodes:
     for k in zones:
-        # if int(i[0]) == int(k+1):
-        #     b.append(int(i))
-        #     x[i,k].start = 1
-        # else:
-        #     x[i,k].start = 0
         x[i,k].start = 1 if int(i[0]) == int(k+1) else 0
-            # w[i,k].start = 1 if int(i[1]) == 0 and int(i[-1]) == int(k) else 0 
-            # y[i,j,k].start = 
 
 # solve model
 model.optimize()
 
 # organize results
+# console output doc: http://www.gurobi.com/documentation/7.0/refman/mip_logging.html
 if model.SolCount == 0:
     print('No solution found, optimization status = %d' % model.Status, file=sys.stderr)
 else:
@@ -123,6 +126,7 @@ else:
             if v.X == 1. and v.VarName[0] == "x":
                 node = v.VarName[2:5]
                 zone = v.VarName[6]
-                fresult.write("%d,%s,%s,%f\n" % (no, node, zone, workloads[node]))
-                print("beat %s in zone %s" % (node, zone), file=sys.stderr)
+                fresult.write("%d,%s,%d,%f\n" % (no, node, int(zone)+1, workloads[node]))
+                print("beat %s in zone %d" % (node, int(zone)+1), file=sys.stderr)
                 no += 1
+            
