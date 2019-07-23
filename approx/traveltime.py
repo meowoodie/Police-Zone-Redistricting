799df5d237ee5f29cf8c4ac16c290df6d72cc81c
meowoodie <woodielove@163.com>
2019-07-23 00:25:10 -0400

    add travel time estimation

--- approx/traveltime.py
+++ approx/traveltime.py
@@ -1,9 +1,31 @@
+import numpy as np
 from collections import defaultdict
 
-traces = defaultdict(list)
-with open("patrol.trace.txt", "r") as f:
+routes = []
+beats  = set()
+with open("data/patrol.route.txt", "r") as f:
     for line in f.readlines():
-        data   = line.strip().split("\t")
-        off_id = data[0]
-        lat, lng, call_t, disp_t, arv_t, clr_t = [ float(d) for d in data[1:] ]
-        print(off_id, lat, lng, call_t, disp_t, arv_t, clr_t)
+        data = line.strip().split("\t")
+        start_beat, end_beat, dt = data[0], data[1], float(data[2])
+        routes.append([start_beat, end_beat, dt])
+        beats = beats | set({start_beat, end_beat})
+
+beats   = list(beats)
+beats.sort()
+n_beats = len(beats) 
+print(beats)
+print(n_beats)
+
+total_t = np.zeros((n_beats, n_beats))
+total_n = np.zeros((n_beats, n_beats))
+for route in routes:
+    start_idx = beats.index(route[0])
+    end_idx   = beats.index(route[1])
+    dt        = route[2]
+    total_t[start_idx][end_idx] += dt
+    total_n[start_idx][end_idx] += 1
+
+Tau = np.divide(total_t, total_n, out=np.zeros_like(total_t), where=total_n!=0)
+print(Tau)
+print((n_beats * n_beats - np.count_nonzero(Tau)) / (n_beats * n_beats))
+np.save("tau_matrix", Tau)
--- casestudy/trace_dots_map.py
+++ casestudy/trace_dots_map.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 #             data     = line.strip().split("\t")
 #             date     = data[2].strip()
 #             call_t   = data[55].strip()
-#             lat, lng = data[8].strip(), data[9].strip()
+#             lat, lng, beat = data[8].strip(), data[9].strip(), data[16].strip()
 #             disp_t, arv_t, clr_t = data[40].strip(), data[42].strip(), data[45].strip()
 #             off_id   = data[50].strip()
 #             if off_id != "" and call_t != "" and lat != "" and lng != "" and disp_t != "" and arv_t != "" and clr_t != "":
@@ -19,75 +19,87 @@ from collections import defaultdict
 #                 arv_t  = date + float(arv_t[:2]) * 3600 + float(arv_t[2:4]) * 60 + float(arv_t[4:])
 #                 clr_t  = date + float(clr_t[:2]) * 3600 + float(clr_t[2:4]) * 60 + float(clr_t[4:])
 #                 if arv_t - disp_t >= 0 and clr_t - arv_t >= 0:
-#                     print("\t".join([ str(d) for d in (off_id, lat, lng, call_t, disp_t, arv_t, clr_t) ]))
+#                     print("\t".join([ str(d) for d in (off_id, lat, lng, beat, call_t, disp_t, arv_t, clr_t) ]))
 #         except Exception as e:
 #             pass
 
 traces = defaultdict(list)
-with open("patrol.trace.txt", "r") as f:
+with open("data/patrol.trace.txt", "r") as f:
     for line in f.readlines():
         data   = line.strip().split("\t")
-        off_id = data[0]
-        lat, lng, call_t, disp_t, arv_t, clr_t = [ float(d) for d in data[1:] ]
+        off_id, lat, lng, beat = data[0], data[1], data[2], data[3]
+        call_t, disp_t, arv_t, clr_t = [ float(d) for d in data[4:] ]
         # print(off_id, lat, lng, call_t, disp_t, arv_t, clr_t)
         traces[off_id].append({
-            "lat": lat, "lng": lng, 
+            "lat": lat, "lng": lng, "beat": beat,
             "call": call_t, "disp": disp_t, "arv": arv_t, "clr": clr_t,
             "dt": arv_t - disp_t})
 
-print(len(traces))
-off_list  = list(traces.keys())
-off_list.sort()
+for officer in traces:
+    n_stops = len(traces[officer])
+    for i in range(n_stops-1):
+        start = traces[officer][i]
+        end   = traces[officer][i+1]
+        if start["beat"] is not "" and end["beat"] is not "":
+            route = [start["beat"], end["beat"], str(start["dt"])]
+            # routes.append(route)
+            print("\t".join(route))
 
-# PLOTTING TRAJECTORIES ON THE MAP
-import branca
-import folium
-import webbrowser
-import numpy as np
-import matplotlib.pyplot as plt
-# configuration
-geojson_path = '/Users/woodie/Desktop/workspace/Zoning-Analysis/data/apd_beat.geojson'
-trace        = traces["3761"]
-center       = [33.796480, -84.394220]
-# data preparation
-times     = np.array([ p["call"] for p in trace ]) # len: N
-travel_t  = np.array([ p["dt"] for p in trace ])   # N
-locations = np.array([ [ p["lat"], -1 * p["lng"] ] for p in trace ]) # N
-t_order   = np.argsort(times)
-travel_t  = travel_t[t_order]                      # N
-locations = locations[t_order]                     # N
-# calculate distances and speeds
-distances = np.array([ # len: N-1
-    abs(locations[i][0] - locations[i-1][0]) + \
-    abs(locations[i][1] - locations[i-1][1]) 
-    for i in range(1, len(locations)) ])       
-speeds    = np.array([ # N-1
-    distances[i] / travel_t[i+1] if travel_t[i+1] != 0 else -1 
-    for i in range(0, len(travel_t) - 1)])     
-idx       = np.array([ # N-1
-    i for i in range(0, len(distances))                 
-    if travel_t[i+1] < 3600 and travel_t[i+1] > 0 ]) # remove zero travel time
+# print(len(traces))
+# off_list = list(traces.keys())
+# off_list.sort()
 
-# print(min(np.exp(speeds[idx])), max(np.exp(speeds[idx])))
-print(min(travel_t[idx+1]), max(travel_t[idx+1]))
+# # PLOTTING TRAJECTORIES ON THE MAP
+# import branca
+# import folium
+# import webbrowser
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-# map initialization
-html_path = 'trace.html'
-cm        = branca.colormap.linear.YlOrRd_09.scale(min(travel_t[idx+1]), max(travel_t[idx+1]))
-m         = folium.Map(
-    location=center, zoom_start=11, zoom_control=True, max_zoom=20, min_zoom=9)
-m.choropleth(
-    geo_data=open(geojson_path).read(),
-    fill_color='YlGn',
-    fill_opacity=0.1,
-    line_opacity=1.,
-    highlight=True)
-# plot dots and lines
-for i in idx:
-    folium.CircleMarker(
-        location=locations[i+1], color="red", radius=.1).add_to(m)
-    folium.PolyLine(
-        [locations[i+1], locations[i]], weight=2., color=cm(travel_t[i+1]), opacity=.8).add_to(m)
-folium.LayerControl().add_to(m)
-m.save(html_path)
+# # configuration
+# geojson_path = '/Users/woodie/Desktop/workspace/Zoning-Analysis/data/apd_beat.geojson'
+# trace        = traces["3761"]
+# center       = [33.796480, -84.394220]
+
+# # data preparation
+# times     = np.array([ p["call"] for p in trace ]) # len: N
+# travel_t  = np.array([ p["dt"] for p in trace ])   # N
+# locations = np.array([ [ p["lat"], -1 * p["lng"] ] for p in trace ]) # N
+# t_order   = np.argsort(times)
+# travel_t  = travel_t[t_order]                      # N
+# locations = locations[t_order]                     # N
+# # calculate distances and speeds
+# distances = np.array([ # len: N-1
+#     abs(locations[i][0] - locations[i-1][0]) + \
+#     abs(locations[i][1] - locations[i-1][1]) 
+#     for i in range(1, len(locations)) ])       
+# speeds    = np.array([ # N-1
+#     distances[i] / travel_t[i+1] if travel_t[i+1] != 0 else -1 
+#     for i in range(0, len(travel_t) - 1)])     
+# idx       = np.array([ # N-1
+#     i for i in range(0, len(distances))                 
+#     if travel_t[i+1] < 3600 and travel_t[i+1] > 0 ]) # remove zero travel time
+
+# # print(min(np.exp(speeds[idx])), max(np.exp(speeds[idx])))
+# print(min(travel_t[idx+1]), max(travel_t[idx+1]))
+
+# # map initialization
+# html_path = 'trace.html'
+# cm        = branca.colormap.linear.YlOrRd_09.scale(min(travel_t[idx+1]), max(travel_t[idx+1]))
+# m         = folium.Map(
+#     location=center, zoom_start=11, zoom_control=True, max_zoom=20, min_zoom=9)
+# m.choropleth(
+#     geo_data=open(geojson_path).read(),
+#     fill_color='YlGn',
+#     fill_opacity=0.1,
+#     line_opacity=1.,
+#     highlight=True)
+# # plot dots and lines
+# for i in idx:
+#     folium.CircleMarker(
+#         location=locations[i+1], color="red", radius=.1).add_to(m)
+#     folium.PolyLine(
+#         [locations[i+1], locations[i]], weight=2., color=cm(travel_t[i+1]), opacity=.8).add_to(m)
+# folium.LayerControl().add_to(m)
+# m.save(html_path)
 
